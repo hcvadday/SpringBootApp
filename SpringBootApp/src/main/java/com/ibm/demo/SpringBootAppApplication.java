c5a3632a4798e122717181d93c413caf9f8be4c2
hcvadday <108428936+hcvadday@users.noreply.github.com>
2022-06-30 12:16:56 +0530

    Update SpringBootAppApplication.java

--- SpringBootApp/src/main/java/com/ibm/demo/SpringBootAppApplication.java
+++ SpringBootApp/src/main/java/com/ibm/demo/SpringBootAppApplication.java
@@ -6,6 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootAppApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootAppApplication.class, args);
+		 SpringApplication.run(SpringBootAppApplication.class, args);
 	}
 }
